6b04905ac4323d695758044515324886eb353f6f
jbui019 <63322322+jbui019@users.noreply.github.com>
2020-05-13 12:43:55 -0700

    Update jbui019_lab6_part3.c

--- jbui019_lab6_part3.c
+++ jbui019_lab6_part3.c
@@ -3,7 +3,7 @@
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *	https://www.youtube.com/watch?v=ndTbWb6rzbM&feature=youtu.be
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
